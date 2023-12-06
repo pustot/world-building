436a49e975bf61d3a90c2f9cc37daf286ac99bf7
yangcxPusto <619980817@qq.com>
2023-12-06 15:14:45 +0000

    rim dhuk: ɨ 用 ï

--- OQeVD8nA.js
+++ OQeVD8nA.js
@@ -2,9 +2,11 @@
  *
  * Han character pronunciation in Ttomniese
  * 
+ * 'řim dħuk
+ * 
  * 老版本存放在 -old.js，但打算直接用切韵拼音的字面音和切韵通俗拟音替代，用作汉字音读之切音。
  * 切音，顾名思义源自切韵音，是中古汉语的代表。
- * 另开此实验性的个人朗读音，因为严格按照切韵拟音太难读了。估计跟什么近古方言都不像。
+ * 另开此实验性的个人朗读音，原則是犧牲擬音之嚴密，而主推幫助記憶音韻地位以及方便朗讀。
  * 暂称为绝呼音。因为绝系语文，或许因为其后起文化以及庞大规模，就喜欢引用以及简化。
  * （或许把切韵音野鸡着读就成了本方案了吧。。。）
  * 基本思路是
@@ -12,9 +14,29 @@
  * - 介音保留 e n 但不保留我个人不太会发的 y u（写出以二等和B类为代表的 Q 但它可能省略或读为 e 或仿古）；
  * - 韵腹尽量取几个现代变体里最常见的（很可能导致合并）；
  * - 声调基本只有平上去入，暂定采用近似菏泽话
+ * - 韻母基本不考慮根據聲母而產生變體，既爲了偷懶也爲了方便記憶音韻地位
  * 不断调整中！
- *
- * @author TwaqNgu
+ * 
+ * 音系小整理
+ * - 韻腹：廣用有八 /a/, /e/, /i/, /o/, /u/, /ə/ ë, /ɨ/ ï, /y/ ü; 罕用有三 /ɛ/ ä, /ɑ/ aħ, å /ɔ/
+ *   - 可見，其元音爲
+ *    - 普世常見之 /a/ /e/ /i/ /o/ /u/，外加
+ *    - 菏、京官話常見之 /ə/ /y/，與可視爲見於菏、京官話的 /ɨ/（舌尖元音，如「思」「之」），外加
+ *      - /ɨ/，國際音標也會以 ï 表示（央化之 i 也），故羅馬化爲 ï
+ *        - 且 /ɨ/ 所出現的之攝、臻攝、流攝皆是與 /i/ 同環境出現，說明古人以攝歸類的時代亦覺二者相似
+ *        - 不用「y」表示，把「y」空出來用於 IPA 風格用字表示 /y/。
+ *    - 只見於唯一一韻或一個搭配的如下幾音（實亦常見與菏、京官話的部分音節或作爲音位變體），發音不準亦無大影響：
+ *      - /ɛ/ ä 僅用於 /ɛŋ/ 即 äň 。即梗攝之庚韻、清韻。以區分於 /eŋ/，其餘情況無需區分
+ *        - （所以非對立的 /ɛ/ 和 /e/ 我還蠻可能發成 /e/。。。唉要不把 /ɛ/ 改成更誇張的 /æ/ 好了）
+ *      - /ɑ/ aħ 僅用於歌韻（即果攝） /ɑ/。即與麻韻（即假攝） /a/ 區分。又因爲麻韻二等前有 ř，歌韻無二等，因此三等開口是唯一區分 /a/ /ɑ/ 的情況（/ia/ /iɑ/），不區分的影響較小。
+ *        - 其實 aň 的韻腹亦實現爲 /ɑ/，但不含對立，故仍寫作 a。
+ *      - /ɔ/ å 僅用於江韻。即 /ʐɔŋ/。由於通攝只有一三等，江攝只有二等，故此韻腹合併入通攝的 /o/ 或 /u/ 區分亦無影響。
+ * - 聲母
+ *    - 濁音：羅馬化時，使用漢語拼音式的拼寫，即不同字母區分送氣清音和不送氣清音，而 ħ 作爲濁音標記。端 d 透 t 定 dħ
+ *      - 是因爲考慮到我本人以及廣大的官話者、粵語者等非吳非閩母語者通常發不準濁音，故給濁音加標記以暗示其在本用戶的懶音中爲可選特性。
+ *      - 同時也跟本人別的造語文字保持一致
+ * 
+ * @author TwaqNgu (Duáħ Ňïu)
  */
 
 const is = (...x) => 音韻地位.屬於(...x);
@@ -22,7 +44,7 @@ const when = (...x) => 音韻地位.判斷(...x);
 
 const is羅馬化 = 選項.羅馬化 ?? false;
 
-if (!音韻地位) return [['羅馬化', is羅馬化]];
+if (!音韻地位) return [['羅馬化', is羅馬化], ['聲調符號', is聲調符號]];
 
 // 音韻地位 = Qieyun.適配分析體系('ytenx')(音韻地位);
 
@@ -71,14 +93,14 @@ function 聲母規則() {
 }
 
 function 韻母規則() {
-  // 通攝 o Z
+  // 通攝 n Z tuň
   if (is('東韻 一等')) return 'nZ';
   if (is('東韻 三等')) return 'enZ';
   if (is('冬韻')) return 'oZ';
   if (is('鍾韻')) return 'eoZ';
-  // 江攝 m Z
+  // 江攝 m Z gřåň
   if (is('江韻')) return 'QmZ'; // Q deleted
-  // 止攝 e
+  // 止攝 e dçï2
   if (is('支韻 合口')) return is('重紐B類') ? 'Qva' : 'va';  // 未标AB的话？ // Q deleted
   if (is('支韻')) return is('重紐B類') ? 'Qea' : 'ea';  // 未标AB的话？ // Q deleted
   if (is('脂韻 合口')) return is('重紐B類') ? 'Qv' : 'v';  // 未标AB的话？ // Q deleted
@@ -86,11 +108,11 @@ function 韻母規則() {
   if (is('之韻')) return 'y';
   if (is('微韻 開口')) return 'ye';
   if (is('微韻')) return 'nye';
-  // 遇攝 o
+  // 遇攝 o ňüo3
   if (is('魚韻')) return 'ei';
   if (is('虞韻')) return 'vo';
   if (is('模韻')) return 'no';
-  // 蟹攝 re
+  // 蟹攝 re xħře2
   if (is('齊韻 合口')) return 'nae';
   if (is('齊韻')) return 'ae';
   if (is('祭韻 合口')) return is('重紐B類') ? 'QvaQ' : 'vaQ'; // Q deleted
@@ -107,7 +129,7 @@ function 韻母規則() {
   if (is('灰韻')) return 'nie';
   if (is('廢韻 開口')) return 'eiQ';
   if (is('廢韻')) return 'voQ';
-  // 臻攝 eH
+  // 臻攝 eH dšřin
   if (is('眞韻 合口')) return is('重紐B類') ? 'QvH' : 'vH'; // Q deleted
   if (is('眞韻')) return is('重紐B類') ? 'QeH' : 'eH'; // Q deleted
   if (is('臻韻')) return 'QeH'; // Q deleted
@@ -117,7 +139,7 @@ function 韻母規則() {
   if (is('元韻')) return 'voH';
   if (is('痕韻')) return 'iH';
   if (is('魂韻')) return 'niH';
-  // 山攝 rH
+  // 山攝 rH šřen
   if (is('寒韻 開口')) return 'rH';
   if (is('寒韻')) return 'nrH';
   if (is('刪韻 合口')) return 'QnrH'; // Q deleted
@@ -128,26 +150,26 @@ function 韻母規則() {
   if (is('仙韻')) return is('重紐B類') ? 'QeaH' : 'eaH'; // Q deleted
   if (is('先韻 合口')) return 'naH';
   if (is('先韻')) return 'aH';
-  // 效攝 rn
+  // 效攝 rn xħřau3
   if (is('蕭韻')) return 'an';
   if (is('宵韻')) return is('重紐B類') ? 'Qean' : 'ean'; // Q deleted
   if (is('肴韻')) return 'Qrn'; // Q deleted
   if (is('豪韻')) return 'rn';
-  // 果攝 rp
+  // 果攝 rp guaħ2
   if (is('歌韻 一等 開口')) return 'rp';
   if (is('歌韻 一等')) return 'nrp';
   if (is('歌韻 三等 開口')) return 'erp';
   if (is('歌韻 三等')) return 'vrp';
-  // 假攝 r
+  // 假攝 r gřa2
   if (is('麻韻 二等 合口')) return 'Qnr'; // Q deleted
   if (is('麻韻 二等')) return 'Qr'; // Q deleted
   if (is('麻韻 三等')) return 'er';
-  // 宕攝 aZ
+  // 宕攝 r Z dħaň3
   if (is('陽韻 開口')) return 'erZ';
   if (is('陽韻')) return 'vrZ';
   if (is('唐韻 合口')) return 'nrZ';
   if (is('唐韻')) return 'rZ';
-  // 梗攝 eZ
+  // 梗攝 a Z gřäň2
   if (is('庚韻 二等 合口')) return 'QncZ'; // Q deleted
   if (is('庚韻 二等')) return 'QcZ'; // Q deleted
   if (is('庚韻 三等 合口')) return 'QvcZ'; // Q deleted
@@ -158,18 +180,18 @@ function 韻母規則() {
   if (is('清韻')) return 'ecZ'; // is('重紐A類') ? 'eLZ' : 'QeLZ';
   if (is('青韻 合口')) return 'naZ';
   if (is('青韻')) return 'aZ';
-  // 曾攝 iZ
+  // 曾攝 i Z dsëň
   if (is('蒸韻 合口')) return 'QvZ'; // Q deleted
   if (is('蒸韻')) return is('重紐B類') ? 'QeZ' : 'eZ';  // TODO: 按声母分 // Q deleted
   if (is('登韻 合口')) return 'niZ';
   if (is('登韻')) return 'iZ';
-  // 流攝 n
+  // 流攝 en lïu
   if (is('尤韻')) return 'yn';
   if (is('侯韻')) return 'n';
   if (is('幽韻')) return  is('重紐B類') ? 'Qen' : 'en'; // Q deleted
-  // 深攝 eV
+  // 深攝 eV çim
   if (is('侵韻')) return is('重紐B類') ? 'QeV' : 'eV'; // Q deleted
-  // 咸攝 rV
+  // 咸攝 rV xħřem
   if (is('覃韻')) return 'iV';
   if (is('談韻')) return 'rV';
   if (is('鹽韻')) return is('重紐B類') ? 'QeaV' : 'eaV'; // Q deleted
@@ -273,7 +295,7 @@ function get羅馬化(result) {
     ["f", "?"], ["g", "?"], ["h", "p"], ["i", "ë"], ["j", "?"],
     ["k", "ŕ"], ["l", "ł"], ["m", "å"], ["n", "u"], ["o", "o"],
     ["p", "ħ"], ["q", "_"], ["r", "a"], ["s", "ö"], ["t", "ę"],
-    ["u", "ÿ"], ["v", "ü"], ["w", "û"], ["x", "h"], ["y", "y"],
+    ["u", "ÿ"], ["v", "ü"], ["w", "û"], ["x", "h"], ["y", "ï"],
     ["z", "æ"],
     ["A", "k"], ["B", "tç"], ["C", "s"], ["D", "d"], ["E", "j"],
     ["F", "v"], ["G", "dš"], ["H", "n"], ["I", "?"], ["J", "tš"],
